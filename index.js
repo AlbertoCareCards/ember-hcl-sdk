19ac2cd86ee71583b0b3aa155c138cc51cb967ea
pascu <Lloyd13Killer783>
2021-06-15 19:13:56 +0200

    - Fixed HCL SDK dependencies.

--- index.js
+++ index.js
@@ -5,11 +5,5 @@ module.exports = {
 
   included: function (app) {
     this._super.included(app);
-    app.import(
-      'https://static.healthcarelocator.com/v1/hcl-sdk-web-ui/hcl-sdk.js'
-    );
-    app.import(
-      'https://static.healthcarelocator.com/v1/hcl-sdk-api/hcl-sdk-api.js'
-    );
   },
 };
