54f2a5a1add405314f8ca00e70f521a57b1df647
pascu <Lloyd13Killer783>
2021-06-15 20:16:17 +0200

    - Added HCL SDK component logic.

--- addon/components/hcl-ui.js
+++ addon/components/hcl-ui.js
@@ -1,4 +1,25 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
 export default class HclUiComponent extends Component {
+  // Component Input Properties
+  // --------------------------
+  apiKey = ''
+  appURL = ''
+  appName = ''
+  screenName = 'searchNearMe'
+  specialist = 'GP'
+
+  @action
+  loadHclSdk(sdkElement) {
+    sdkElement.init({
+      apiKey: this.args.apiKey,
+      appName: this.args.appName,
+      appURL: this.args.appURL,
+      entry: {
+        screenName: this.args.screenName,
+        specialtyCode: this.specialist
+      }
+    })
+  }
 }
